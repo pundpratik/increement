3ffe17f2fd691fe396341d1b082330c9b4930db7
Pratik Pund <88658436+pundpratik@users.noreply.github.com>
2021-08-18 18:06:28 +0530

    increement

---
+++ args.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int c=10;
+	printf("\n %d\t%d\t%d\t",c,c++,++c);
+	return 0;
+}
+/*#include<stdio.h>
+int main()
+{
+	int c=10;
+	printf("\n %d\t %d\t%d",c,++c,c++);
+	return 0;
+}*/
